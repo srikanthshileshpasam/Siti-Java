ed5fd916e285059022d5f507ef9917b8815efa45
Srikanth Pasam <10387794@mydbs.ie>
2020-01-29 12:09:38 +0000

    toggle

--- Webpages/toggle.js
+++ Webpages/toggle.js
@@ -1,8 +1,10 @@
+window.onload = function() {
+  document.getElementById('toggleDIV').style.display = 'none';
+};
+
 function toggle() {
   var x = document.getElementById("toggleDIV");
   if (x.style.display === "none") {
     x.style.display = "block";
-  } else {
-    x.style.display = "none";
   }
 }
