9752cccf7e4a54bfc7a6aeae98741190ef24c5ef
Srikanth Pasam <10387794@mydbs.ie>
2020-01-29 11:55:28 +0000

    toggle

---
+++ Webpages/toggle.js
@@ -0,0 +1,8 @@
+function toggle() {
+  var x = document.getElementById("toggleDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
